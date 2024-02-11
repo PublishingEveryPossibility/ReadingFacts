b1df996456ef9fede86fc0f1130f4998d78d6bc5
Josiah Walker <118691621+simplyjosiah@users.noreply.github.com>
2024-02-11 18:33:21 -0500

    updated 'ReadMe'

--- reading.py
+++ reading.py
@@ -1,16 +1,4 @@
-"""
-Encouraging users to read more to improve their quality of life through reading stats. 
 
-Setting daily goals will help improve literacy, mental health, self-esteem, lower stress, and improve rest using the program written in Python.
-
-The program will allow the user to input their age and the amount of time they have resourced to enjoy leisure reading. 
-The program will return statistics that will show their words per minute average, while displaying statistical integers reflecting a percentage on rest improvement and stress reduction.
-
-All of the information needed to create the formulas to return accurate information will be sited from -
-
-‘http://www.crossrivertherapy.com/research/reading-statistics’ .
-
-"""
 user_age = 0 #where user age is stored
 time_of_day = '' # answer is stored
 user_reading= 0 #user reading time is stored
